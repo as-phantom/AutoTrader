5b8e89c9fa3d7a5b74e4995090bbc7b1fda46423
Alexander Stoev <stoev95pd@gmail.com>
2021-08-04 17:00:03 +0300

    Header, Footer, Not-Found & Auth components

--- src/app/modules/core/components/not-found/not-found.component.ts
+++ src/app/modules/core/components/not-found/not-found.component.ts
@@ -4,17 +4,14 @@ import { Auth } from 'aws-amplify';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.sass'],
 })
 export class NotFoundComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   logout() {
     Auth.signOut();
   }
-
 }
