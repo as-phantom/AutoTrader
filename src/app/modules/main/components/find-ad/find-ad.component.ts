6a65041ab9cb021cae986879629f1eec63765462
Alexander Stoev <stoev95pd@gmail.com>
2021-08-07 01:35:52 +0300

    Form added in FindAdComponent

--- src/app/modules/main/components/find-ad/find-ad.component.ts
+++ src/app/modules/main/components/find-ad/find-ad.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-find-ad',
   templateUrl: './find-ad.component.html',
-  styleUrls: ['./find-ad.component.sass']
+  styleUrls: ['./find-ad.component.sass'],
 })
-export class FindAdComponent implements OnInit {
+export class FindAdComponent {
+  public searchForm: FormGroup;
+  public submitted: boolean = false;
+  public isFormValid: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public search: FormBuilder) {
+    this.searchForm = this.search.group({
+      make: [''],
+      model: [''],
+      region: [''],
+      minPrice: [''],
+      maxPrice: [''],
+      condition: [''],
+    });
   }
 
+  onSubmit() {
+    console.log(this.searchForm.value);
+
+    // this.submitted = true;
+  }
 }
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -5,11 +5,26 @@ import { FindAdComponent } from './components/find-ad/find-ad.component';
 import { LatestOffersComponent } from './components/latest-offers/latest-offers.component';
 import { PlaceAdComponent } from './components/place-ad/place-ad.component';
 import { HomeComponent } from './components/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Materials
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 const COMPONENTS = [HomeComponent, FindAdComponent, LatestOffersComponent, PlaceAdComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule, MainRoutingModule],
+  imports: [
+    CommonModule,
+    MainRoutingModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+  ],
 })
 export class MainModule {}
