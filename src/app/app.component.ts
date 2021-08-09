1350fa4b3d4768e4bd0af47ef43bf8d8470054e6
Alexander Stoev <stoev95pd@gmail.com>
2021-08-09 23:31:59 +0300

    Loading entities function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { AuthService } from './modules/core/services/auth.service';
+import { RegionsFacade } from './store/facades/regions.facade';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +11,17 @@ import { AuthService } from './modules/core/services/auth.service';
 export class AppComponent {
   title = 'AutoTrader';
 
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService, private readonly regionsFacade: RegionsFacade) {}
 
   public ngOnInit(): void {
     // Store the cognito user
     this.authService.storeCurrentAuthenticatedUser();
+
+    // Load various entities that the app relies on upon initial load
+    this.loadEntities();
+  }
+
+  private loadEntities(): void {
+    this.regionsFacade.loadRegions(environment.defaultAppSyncMaxPaginationLimit, null);
   }
 }
