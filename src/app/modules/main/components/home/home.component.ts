9f69269c8a2c6af432eb2e2cd3455aea4b5828cb
Alexander Stoev <stoev95pd@gmail.com>
2021-08-09 18:28:17 +0300

    Testing something

--- src/app/modules/main/components/home/home.component.ts
+++ src/app/modules/main/components/home/home.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { API } from 'aws-amplify';
+import gql from 'graphql-tag';
+import { Region } from 'src/API';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.sass'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit (): void {
+    const createRegion = async (region: string): Promise<Region> => {
+      const name = region;
 
-  ngOnInit(): void {
+      const response = await API.graphql({
+        query: gql`
+          mutation CreateRegion($input: CreateRegionInput!) {
+            createRegion(input: $input) {
+              id
+              name
+              createdAt
+              updatedAt
+            }
+          }
+        `,
+        variables: {
+          input: {
+            name,
+          },
+        },
+      });
+
+      const {
+        data: { createRegion },
+      } = response as { data: { createRegion: Region } };
+
+      return createRegion;
+    };
+
+   createRegion('Plovdiv').then(console.log).catch(console.error);
+   
   }
-
 }
