353e0396453f6df3f1aeb84ac886a2ad69ba257c
Alexander Stoev <stoev95pd@gmail.com>
2021-08-11 17:57:29 +0300

    Utilities

---
+++ src/app/modules/core/services/utilities.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UtilitiesService {
+  static validate(value: any, validator: (v: any) => boolean): boolean {
+    return validator(value);
+  }
+
+  constructor() {}
+}
