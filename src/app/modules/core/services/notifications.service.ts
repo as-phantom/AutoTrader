63114962ee3cc7c2265ab3d35a5bd28da21e391a
Alexander Stoev <stoev95pd@gmail.com>
2021-08-12 16:14:55 +0300

    Notification service updated

--- src/app/modules/core/services/notifications.service.ts
+++ src/app/modules/core/services/notifications.service.ts
@@ -3,37 +3,28 @@ import { ToastrService } from 'ngx-toastr';
 
 @Injectable({ providedIn: 'root' })
 export class NotificationsService {
+  private readonly notificationsConfigurations: { progressBar: boolean; closeButton: boolean; positionClass: string } =
+    {
+      progressBar: true,
+      closeButton: true,
+      positionClass: 'toast-top-right',
+    };
+
   constructor(private readonly toastrService: ToastrService) {}
 
   success(message: string) {
-    this.toastrService.success(message, 'Success', {
-      progressBar: true,
-      closeButton: true,
-      positionClass: 'toast-top-right',
-    });
+    this.toastrService.success(message, 'Success', this.notificationsConfigurations);
   }
 
   info(message: string) {
-    this.toastrService.info(message, 'Info', {
-      progressBar: true,
-      closeButton: true,
-      positionClass: 'toast-top-right',
-    });
+    this.toastrService.info(message, 'Info', this.notificationsConfigurations);
   }
 
   warning(message: string) {
-    this.toastrService.warning(message, 'Warning', {
-      progressBar: true,
-      closeButton: true,
-      positionClass: 'toast-top-right',
-    });
+    this.toastrService.warning(message, 'Warning', this.notificationsConfigurations);
   }
 
   error(message: string) {
-    this.toastrService.error(message, 'Error', {
-      progressBar: true,
-      closeButton: true,
-      positionClass: 'toast-top-right',
-    });
+    this.toastrService.error(message, 'Error', this.notificationsConfigurations);
   }
 }
