d2b935aca57d3c6b4302036a73599188e4ce66a5
Alexander Stoev <stoev95pd@gmail.com>
2021-08-13 22:55:12 +0300

    Routes for ads created

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdDetailsComponent } from '../shared/components/ad-details/ad-details.component';
+import { AdsListComponent } from '../shared/components/ads-list/ads-list.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
@@ -7,6 +9,15 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: HomeComponent,
+  },
+
+  {
+    path: 'ads',
+    component: AdsListComponent,
+  },
+  {
+    path: 'ads/:id',
+    component: AdDetailsComponent,
   },
 ];
 
