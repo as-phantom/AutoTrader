22e136a3c89126c2437fce2e150118cb4db6b2d9
Alexander Stoev <stoev95pd@gmail.com>
2021-08-13 22:54:19 +0300

    Loading recent 10 posted ads

--- src/app/modules/main/components/latest-offers/latest-offers.component.ts
+++ src/app/modules/main/components/latest-offers/latest-offers.component.ts
@@ -1,41 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AdsFacade } from 'src/app/store/facades/ads.facade';
 
 @Component({
   selector: 'app-latest-offers',
   templateUrl: './latest-offers.component.html',
   styleUrls: ['./latest-offers.component.sass'],
 })
-export class LatestOffersComponent implements OnInit {
-  public images: object[] | [] = [
-    {
-      path: 'https://cdn.audi.bg/media/Theme_Menu_Model_Dropdown_Item_Image_Component/root-bg-model-modelMenu-editableItems-20246-dropdown-413731-image/dh-487-a0e9a6/14357725/1627885145/audi-galerie-5-oe.jpg',
-    },
+export class LatestOffersComponent implements OnInit, OnDestroy {
+  private readonly subscriptions: Subscription[] = [];
+  public images: Object[] = [];
 
-    {
-      path: 'https://cdn.audi.at/media/Theme_Menu_Model_Dropdown_Item_Image_Component/root-at-model-modelMenu-editableItems-25-dropdown-332540-image/dh-487-a0e9a6/32874597/1627376329/audi-a5-coupe-2020.jpg',
-    },
-
-    { path: 'https://www.motoreport.de/wp-content/uploads/2019/05/audi-e-tron-GT-810x456.jpg' },
-
-    {
-      path: 'https://i.auto-bild.de/ir_img/2/6/6/1/3/7/9/Audi-Functions-on-Demand-Fahrzeugfunktionen-zum-nachbuchen-474x316-864346a3a01476b0.jpg',
-    },
-
-    { path: 'https://www.motoreport.de/wp-content/uploads/2019/05/audi-e-tron-GT-810x456.jpg' },
-  ];
-
-  public loop: boolean = true;
-  public autoplay: boolean = true;
+  public arrowsOutside: boolean = true;
   public pauseOnHover: boolean = true;
-  public cellsToShow: number = 3;
-  public borderRadius: number = 10;
+  public autoplay: boolean = true;
+  public loop: boolean = true;
+  public height: number = window.innerHeight * 0.25;
   public autoplayInterval: number = 4000;
   public transitionDuration: number = 500;
-  public width: number = (screen.width | 0) - 550;
-  public height: number = window.innerHeight * 0.25;
-  public arrowsOutside: boolean = true;
+  public borderRadius: number = 10;
+  public cellsToShow: number = 3;
 
-  constructor() {}
+  constructor(
+    private readonly adsFacade: AdsFacade,
+    private readonly changeDetectorRef: ChangeDetectorRef,
+    private readonly router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.adsFacade.ads$.subscribe((ads) => {
+        if (!ads) {
+          return;
+        }
+
+        this.images = ads
+          .slice()
+          .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+          .map((ad) => ({ path: ad.picture }))
+          .slice(0, 10);
+
+        this.changeDetectorRef.detectChanges();
+
+        document.querySelectorAll('carousel img').forEach((img) => {
+          img.addEventListener('click', (e) => {
+            const img = (e.target as HTMLImageElement).src;
+            const ad = ads.find((ad) => ad.picture === img);
+
+            if (!ad) {
+              return;
+            }
+
+            this.router.navigate([`/ads/${ad.id}`]);
+          });
+        });
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
 }
