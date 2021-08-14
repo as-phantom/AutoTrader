cc05ef6af9771d0cfc16a9df481ccfa71639ab25
Alexander Stoev <stoev95pd@gmail.com>
2021-08-14 19:19:35 +0300

    Check for owner

--- src/app/modules/shared/components/ad-details/ad-details.component.ts
+++ src/app/modules/shared/components/ad-details/ad-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
-import { Ad } from 'src/API';
+import { Ad, User } from 'src/API';
 import { AdsService } from 'src/app/modules/core/services/ads.service';
+import { AuthFacade } from 'src/app/store/facades/auth.facade';
 
 @Component({
   selector: 'app-ad-details',
@@ -12,13 +13,19 @@ import { AdsService } from 'src/app/modules/core/services/ads.service';
 export class AdDetailsComponent implements OnInit, OnDestroy {
   private readonly subscriptions: Subscription[] = [];
   public ad$: Observable<Ad | null> | undefined;
+  public user$: Observable<User | undefined> | undefined;
 
-  constructor(private readonly route: ActivatedRoute, private readonly adsService: AdsService) {}
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly adsService: AdsService,
+    private readonly authFacade: AuthFacade
+  ) {}
 
   ngOnInit(): void {
     this.subscriptions.push(
       this.route.params.subscribe((params: Params) => {
         this.ad$ = this.adsService.loadAdById(params.id);
+        this.user$ = this.authFacade.user$;
       })
     );
   }
