062a41d008764c2ccd7a19adb69b2110b7071889
Alexander Stoev <stoev95pd@gmail.com>
2021-08-14 16:57:35 +0300

    Loading service done and implemented in AdsList and AdDetails components

---
+++ src/app/modules/core/services/load-data.service.ts
@@ -0,0 +1,151 @@
+import { Injectable } from '@angular/core';
+import { API } from 'aws-amplify';
+import gql from 'graphql-tag';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Ad, Condition } from 'src/API';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoadDataService {
+  constructor() {}
+
+  public loadAdById(id: string): Observable<Ad | null> {
+    return from(
+      API.graphql({
+        query: gql`
+          query GetAd($id: ID!) {
+            getAd(id: $id) {
+              id
+              make
+              model
+              color
+              engine
+              price
+              year
+              mileage
+              region {
+                id
+                name
+              }
+              favorites {
+                items {
+                  id
+                }
+              }
+              pictures {
+                items {
+                  url
+                }
+              }
+              ratings {
+                items {
+                  rating
+                }
+              }
+              transmissionType
+              description
+              fuelType
+              phone
+              picture
+              condition
+              longitude
+              latitude
+            }
+          }
+        `,
+        variables: {
+          id,
+        },
+      }) as Promise<{ data: { getAd: Ad | null } }>
+    ).pipe(map(({ data: { getAd } }) => getAd));
+  }
+
+  public loadAdsByFilters({
+    region,
+    condition,
+    make,
+    model,
+    minPrice,
+    maxPrice,
+  }: {
+    region: string;
+    condition: Condition;
+    make: string;
+    model: string;
+    minPrice: string;
+    maxPrice: string;
+  }): Observable<Ad[] | null> {
+    const minPriceFilter = minPrice ? { price: { ge: Number(minPrice) } } : {};
+    const maxPriceFilter = maxPrice ? { price: { le: Number(maxPrice) } } : {};
+    const priceFilter = { price: { ...minPriceFilter.price, ...maxPriceFilter.price } };
+
+    const filter = {
+      ...(Object.keys(priceFilter.price).length ? priceFilter : {}),
+      ...(condition ? { condition: { eq: condition } } : {}),
+      ...(make ? { make: { eq: make } } : {}),
+      ...(model ? { model: { eq: model } } : {}),
+      ...(region ? { regionsID: { eq: region } } : {}),
+    };
+
+    return from(
+      API.graphql({
+        query: gql`
+          query ListAds($filter: ModelAdFilterInput) {
+            listAds(filter: $filter) {
+              items {
+                id
+                make
+                model
+                color
+                engine
+                price
+                year
+                mileage
+                region {
+                  id
+                  name
+                }
+                favorites {
+                  items {
+                    id
+                  }
+                }
+                pictures {
+                  items {
+                    url
+                  }
+                }
+                ratings {
+                  items {
+                    rating
+                  }
+                }
+                transmissionType
+                description
+                fuelType
+                phone
+                picture
+                condition
+                longitude
+                latitude
+              }
+            }
+          }
+        `,
+        variables: {
+          ...(Object.keys(filter).length ? { filter } : {}),
+        },
+      }) as Promise<{ data: { listAds: { items: Ad[] | null } } }>
+    ).pipe(
+      map(
+        ({
+          data: {
+            listAds: { items },
+          },
+        }) => items
+      )
+    );
+  }
+}
