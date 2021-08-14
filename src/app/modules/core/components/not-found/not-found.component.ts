2f3f966b5f8d1c5cd7669c59e76b88134488cfb8
Alexander Stoev <stoev95pd@gmail.com>
2021-08-14 18:38:11 +0300

    Not found page done

--- src/app/modules/core/components/not-found/not-found.component.ts
+++ src/app/modules/core/components/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faQuestionCircle, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-not-found',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found.component.sass'],
 })
 export class NotFoundComponent implements OnInit {
+  public get faQuestion(): IconDefinition {
+    return faQuestionCircle;
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
