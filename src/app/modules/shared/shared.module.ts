655bcf5cda0313cae6618dd7c4786b876ffd538e
Alexander Stoev <stoev95pd@gmail.com>
2021-08-18 17:24:36 +0300

    Housekeeping and minor styling done

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatRippleModule } from '@angular/material/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 // Components
 import { AdCardBigComponent } from './components/ad-card-big/ad-card-big.component';
 import { AdCardSmallComponent } from './components/ad-card-small/ad-card-small.component';
@@ -11,6 +12,7 @@ import { LoadingComponent } from './components/loading/loading.component';
 import { NoResultsComponent } from './components/no-results/no-results.component';
 import { RatingComponent } from './components/rating/rating.component';
 import { SocialFabIconComponent } from './components/social-fab-icon/social-fab-icon.component';
+import { CurrencyPipe } from './pipes/currency.pipe';
 
 const COMPONENTS = [
   AdCardBigComponent,
@@ -20,10 +22,13 @@ const COMPONENTS = [
   NoResultsComponent,
   RatingComponent,
   SocialFabIconComponent,
+  CurrencyPipe,
 ];
 
+const MODULES = [CommonModule, FontAwesomeModule, MatRippleModule, MatProgressSpinnerModule, IvyCarouselModule];
+
 @NgModule({
-  imports: [CommonModule, FontAwesomeModule, MatRippleModule, MatProgressSpinnerModule],
+  imports: [...MODULES],
   declarations: [...COMPONENTS],
   exports: [...COMPONENTS],
 })
