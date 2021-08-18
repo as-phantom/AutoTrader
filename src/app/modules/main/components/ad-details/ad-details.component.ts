a05ab8381180c81a3689867d9406211fb9e06672
Alexander Stoev <stoev95pd@gmail.com>
2021-08-18 23:51:43 +0300

    Ad details component done

---
+++ src/app/modules/main/components/ad-details/ad-details.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { Ad, User } from 'src/API';
+import { AdsService } from 'src/app/modules/core/services/ads.service';
+import { AuthFacade } from 'src/app/store/facades/auth.facade';
+
+@Component({
+  selector: 'app-ad-details',
+  templateUrl: './ad-details.component.html',
+  styleUrls: ['./ad-details.component.sass'],
+})
+export class AdDetailsComponent implements OnInit, OnDestroy {
+  private readonly subscriptions: Subscription[] = [];
+  public pictures: string[] | null[] | undefined;
+  public ad$: Observable<Ad | null> | undefined;
+  public user$: Observable<User | undefined> | undefined;
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly adsService: AdsService,
+    private readonly authFacade: AuthFacade
+  ) {}
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.route.params.subscribe((params: Params) => {
+        this.ad$ = this.adsService.loadAdById(params.id);
+        this.user$ = this.authFacade.user$;
+
+        // Get the pictures so we can set them in the carousel
+      })
+    );
+
+    // this.ad$?.subscribe((x) => console.log(x));
+    // this.ad$?.subscribe((ad) => (this.pictures = [...ad?.pictures?.items, ad?.picture]));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
+}
