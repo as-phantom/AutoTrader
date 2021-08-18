4e0a74d07e5df6f5d750f6ffacf0f42266a2131c
Alexander Stoev <stoev95pd@gmail.com>
2021-08-18 17:17:56 +0300

    Form validation and notifications for main componenets done

--- src/app/modules/main/components/find-ads/find-ads.component.ts
+++ src/app/modules/main/components/find-ads/find-ads.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { faQuestionCircle, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { Observable } from 'rxjs';
 import { Ad, Condition, Region } from 'src/API';
+import { NotificationsService } from 'src/app/modules/core/services/notifications.service';
 import { AdsFacade } from 'src/app/store/facades/ads.facade';
 import { RegionsFacade } from 'src/app/store/facades/regions.facade';
 
@@ -13,9 +14,8 @@ import { RegionsFacade } from 'src/app/store/facades/regions.facade';
   styleUrls: ['./find-ads.component.sass'],
 })
 export class FindAdsComponent implements OnInit {
-  public submitted: boolean = false;
-  public showInfoBox: boolean = false;
-  public isFormValid: boolean = false;
+  public showInfoBox: boolean | undefined;
+  public isFormValid: boolean | undefined;
   public condition = Condition;
 
   public ads$: Observable<Ad[] | undefined> | undefined;
@@ -29,12 +29,13 @@ export class FindAdsComponent implements OnInit {
     make: new FormControl(),
     model: new FormControl(),
     region: new FormControl(),
-    minPrice: new FormControl('', [Validators.min(0)]),
-    maxPrice: new FormControl('', [Validators.max(10000000)]),
+    minPrice: new FormControl('', [Validators.min(0), Validators.max(10000000)]),
+    maxPrice: new FormControl('', [Validators.min(0), Validators.max(10000000)]),
     condition: new FormControl(),
   });
 
   constructor(
+    private readonly notificationService: NotificationsService,
     private readonly regionsFacade: RegionsFacade,
     private readonly adsFacade: AdsFacade,
     private readonly router: Router
@@ -53,8 +54,15 @@ export class FindAdsComponent implements OnInit {
     this.showInfoBox = false;
   }
 
+  public verifyIfMakeIsPicked(): void {
+    const make = this.formGroup.controls.make.value;
+    if (!make) {
+      this.notificationService.error('Please chose make before choosing a model.');
+    }
+  }
+
   public onSubmit(): void {
-    this.submitted = true;
+    this.isFormValid = true;
 
     const make = this.formGroup.controls.make.value;
     const model = this.formGroup.controls.model.value;
@@ -62,6 +70,37 @@ export class FindAdsComponent implements OnInit {
     const minPrice = this.formGroup.controls.minPrice.value;
     const maxPrice = this.formGroup.controls.maxPrice.value;
     const condition = this.formGroup.controls.condition.value;
+
+    // Validate group controls
+    (() => {
+      // Validates both negative values and non numeric values for minPrice
+      if (minPrice.split('').map(isNaN).includes(true)) {
+        this.notificationService.error("Min price can't contain characters neither can be a negative number.");
+        this.isFormValid = false;
+      }
+
+      // Validates maximum min value for minPrice
+      if (minPrice > 10000000) {
+        this.notificationService.error("Min price can't be over ten millions.");
+        this.isFormValid = false;
+      }
+
+      // Validates both negative values and non numeric values for maxPrice
+      if (maxPrice.split('').map(isNaN).includes(true)) {
+        this.notificationService.error("Max price can't contain characters neither can be a negative number!");
+        this.isFormValid = false;
+      }
+
+      // Validates maximum max value for maxPrice
+      if (maxPrice > 10000000) {
+        this.notificationService.error("Max price can't be over ten millions!");
+        this.isFormValid = false;
+      }
+    })();
+
+    if (!this.isFormValid) {
+      return;
+    }
 
     const queryParams = {
       make,
--- src/app/modules/main/components/place-ad/place-ad.component.ts
+++ src/app/modules/main/components/place-ad/place-ad.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Region } from 'src/API';
+import { NotificationsService } from 'src/app/modules/core/services/notifications.service';
 import { RegionsFacade } from 'src/app/store/facades/regions.facade';
 
 @Component({
@@ -10,33 +12,52 @@ import { RegionsFacade } from 'src/app/store/facades/regions.facade';
   styleUrls: ['./place-ad.component.sass'],
 })
 export class PlaceAdComponent implements OnInit {
-  public submitted: boolean = false;
-  public showInfoBox: boolean = false;
-  public isFormValid: boolean = false;
+  public isFormValid: boolean | undefined;
 
-  public makes: string[] | [] = [];
   public regions$: Observable<Region[] | undefined> | undefined;
 
   public readonly formGroup: FormGroup = new FormGroup({
-    region: new FormControl('', []),
-    price: new FormControl('', []),
+    region: new FormControl(),
+    price: new FormControl('', [Validators.min(0), Validators.max(10000000)]),
   });
 
-  constructor(private readonly regionsFacade: RegionsFacade) {}
-
-  public onHoverInfo(): void {
-    this.showInfoBox = true;
-  }
-
-  public onLeaveInfo(): void {
-    this.showInfoBox = false;
-  }
-
-  public onSubmit(): void {
-    // this.submitted = true;
-  }
+  constructor(
+    private readonly notificationService: NotificationsService,
+    private readonly regionsFacade: RegionsFacade,
+    private readonly router: Router
+  ) {}
 
   ngOnInit(): void {
     this.regions$ = this.regionsFacade.regions$.pipe();
+  }
+
+  public onSubmit(): void {
+    this.isFormValid = true;
+
+    const price = this.formGroup.controls.price.value;
+    const region = this.formGroup.controls.region.value;
+
+    // Validate group controls
+    (() => {
+      // Validates both negative values and non numeric values for price
+      if (price.split('').map(isNaN).includes(true)) {
+        this.notificationService.error("Price can't contain characters neither can be a negative number.");
+        this.isFormValid = false;
+      }
+
+      // Validates maximum value for price
+      if (price > 10000000) {
+        this.notificationService.error("Price can't be over ten millions.");
+        this.isFormValid = false;
+      }
+    })();
+
+    if (!this.isFormValid) {
+      return;
+    }
+
+    const queryParams = { price, region };
+
+    this.router.navigate(['admin/create'], { queryParams });
   }
 }
