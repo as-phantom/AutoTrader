4735608cba145142b08d856443e727b10368cceb
Alexander Stoev <stoev95pd@gmail.com>
2021-08-20 00:34:11 +0300

    No results component plugged in My ads component

--- src/app/modules/admin/components/my-ads/my-ads.component.ts
+++ src/app/modules/admin/components/my-ads/my-ads.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { Ad } from 'src/API';
 import { AdsService } from 'src/app/modules/core/services/ads.service';
@@ -12,10 +12,17 @@ import { AuthFacade } from 'src/app/store/facades/auth.facade';
 export class MyAdsComponent implements OnInit {
   private readonly subscriptions: Subscription[] = [];
   public myAds$: Observable<Ad[] | null> | undefined;
+  public host: string | undefined;
 
-  constructor(private readonly adsService: AdsService, private readonly authFacade: AuthFacade) {}
+  constructor(
+    private readonly adsService: AdsService,
+    private readonly authFacade: AuthFacade,
+    private readonly elemRef: ElementRef
+  ) {}
 
   ngOnInit(): void {
+    this.host = this.elemRef.nativeElement.tagName.toLowerCase();
+
     this.subscriptions.push(
       this.authFacade.user$.subscribe((user) => {
         if (user) {
