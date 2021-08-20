955088443aef02627d7714dd132d0602b6e52fa1
Alexander Stoev <stoev95pd@gmail.com>
2021-08-20 21:59:52 +0300

    Big ad card improved

--- src/app/modules/shared/components/ad-card-big/ad-card-big.component.ts
+++ src/app/modules/shared/components/ad-card-big/ad-card-big.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { faMapMarkerAlt, faHeartbeat, faPhone, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import {
+  faMapMarkerAlt,
+  faHeartbeat,
+  faPhone,
+  faEdit,
+  faTrash,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 import { Ad, User } from 'src/API';
 
 @Component({
@@ -11,6 +18,11 @@ import { Ad, User } from 'src/API';
 export class AdCardBigComponent implements OnInit {
   @Input() ad$: Observable<Ad | null> | undefined;
   @Input() user$: Observable<User | undefined> | undefined;
+
+  public pictures: { path: string }[] | undefined;
+  public borderRadius: number = 10;
+  public cellsToShow: number = 1;
+  // public height: number = window.innerHeight * 0.25;
 
   public get faMapMarker(): IconDefinition {
     return faMapMarkerAlt;
@@ -24,7 +36,22 @@ export class AdCardBigComponent implements OnInit {
     return faPhone;
   }
 
+  public get faEdit(): IconDefinition {
+    return faEdit;
+  }
+
+  public get faTrash(): IconDefinition {
+    return faTrash;
+  }
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.ad$?.subscribe((ad) => {
+      if (ad) {
+        this.pictures = [{ path: ad.picture }];
+        ad.pictures?.items?.forEach((p) => this.pictures?.push({ path: p!.url }));
+      }
+    });
+  }
 }
