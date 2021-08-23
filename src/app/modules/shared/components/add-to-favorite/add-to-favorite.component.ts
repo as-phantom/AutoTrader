18c44f0b40de3fd6afec5bdc0ea109a84b41a056
Alexander Stoev <stoev95pd@gmail.com>
2021-08-23 22:03:18 +0300

    Add to favorites component done

--- src/app/modules/shared/components/add-to-favorite/add-to-favorite.component.ts
+++ src/app/modules/shared/components/add-to-favorite/add-to-favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { EventEmitter, Component, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-to-favorite',
@@ -6,9 +6,18 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./add-to-favorite.component.sass'],
 })
 export class AddToFavorite implements OnInit {
+  public readonly HEART_SIZE: number = 24;
+
   @Input() filled: boolean = false;
+  @Input() loading: boolean = false;
+
+  @Output() add: EventEmitter<void> = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  public onAdd(): void {
+    this.add.emit();
+  }
 }
