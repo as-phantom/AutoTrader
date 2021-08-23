5e4e9abf5f2146d7ed2654a0a893c67b264c3f4e
Alexander Stoev <stoev95pd@gmail.com>
2021-08-23 19:06:41 +0300

    Rating service created

---
+++ src/app/modules/core/services/rating.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { API } from 'aws-amplify';
+import gql from 'graphql-tag';
+import { from, Observable } from 'rxjs';
+import { Rating } from 'src/API';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RatingService {
+  constructor() {}
+
+  public createRating(input: {
+    id: string;
+    userID: string;
+    adID: string;
+    rating: number;
+  }): Observable<{ data: { updateRating: Rating | null } }> {
+    return from(
+      API.graphql({
+        query: gql`
+          mutation UpdateRating($input: UpdateRatingInput!) {
+            updateRating(input: $input) {
+              ad {
+                ratings {
+                  items {
+                    id
+                    userID
+                    rating
+                  }
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          input,
+        },
+      }) as Promise<{ data: { updateRating: Rating | null } }>
+    );
+  }
+
+  public updateRating(input: {
+    userID: string;
+    adID: string;
+    rating: number;
+  }): Observable<{ data: { createRating: Rating | null } }> {
+    return from(
+      API.graphql({
+        query: gql`
+          mutation CreateRating($input: CreateRatingInput!) {
+            createRating(input: $input) {
+              ad {
+                ratings {
+                  items {
+                    id
+                    userID
+                    rating
+                  }
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          input,
+        },
+      }) as Promise<{ data: { createRating: Rating | null } }>
+    );
+  }
+}
