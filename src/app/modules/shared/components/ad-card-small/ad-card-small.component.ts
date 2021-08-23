444102853f36fed58532b2e901317934da7c1330
Alexander Stoev <stoev95pd@gmail.com>
2021-08-23 18:05:31 +0300

    Small ad card almost done

--- src/app/modules/shared/components/ad-card-small/ad-card-small.component.ts
+++ src/app/modules/shared/components/ad-card-small/ad-card-small.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { API } from 'aws-amplify';
 import gql from 'graphql-tag';
+import { from } from 'rxjs';
+import { NotificationsService } from 'src/app/modules/core/services/notifications.service';
 import { Ad, Rating, User } from '../../../../../API';
 
 @Component({
@@ -10,12 +12,12 @@ import { Ad, Rating, User } from '../../../../../API';
   styleUrls: ['./ad-card-small.component.sass'],
 })
 export class AdCardSmallComponent implements OnInit {
-  @Input() public user: User | null | undefined;
-  @Input() public ad: Ad | undefined;
+  @Input() public user: User | undefined;
+  @Input() public ad!: Ad;
 
-  // @Output() public adUpdated: EventEmitter<Ad> = new EventEmitter();
+  public loading: boolean = false;
 
-  constructor(private readonly router: Router) {}
+  constructor(private readonly notificationsService: NotificationsService, private readonly router: Router) {}
 
   ngOnInit(): void {}
 
@@ -26,9 +28,8 @@ export class AdCardSmallComponent implements OnInit {
     return totalRating / totalRatingsCount;
   }
 
-  public get userHasAlreadyRated(): boolean {
-    return false;
-    return Boolean(this.user?.ratings?.items?.find((i) => i?.adID === this.ad?.id));
+  public get currentUserRating(): Rating | null | undefined {
+    return this.ad?.ratings?.items?.find((i) => i?.userID === this.user?.id);
   }
 
   public getBackgroundImage(ad: Ad): string {
@@ -37,24 +38,92 @@ export class AdCardSmallComponent implements OnInit {
 
   public onRate(rating: number): void {
     if (!this.user) {
-      this.router.navigate(['auth']);
+      this.router.navigate(['/auth']);
+
+      return;
+    }
+    
+    if (this.user.id === this.ad.userID) {
+      this.notificationsService.info(`You cannot rate your own ads!`);
+
+      return;
     }
 
-    API.graphql({
-      query: gql`
-        mutation CreateRating($input: CreateRatingInput) {
-          createRating(input: $input) {
-            ad
+    this.loading = true;
+
+    if (this.currentUserRating) {
+      const request = API.graphql({
+        query: gql`
+          mutation UpdateRating($input: UpdateRatingInput!) {
+            updateRating(input: $input) {
+              ad {
+                ratings {
+                  items {
+                    id
+                    userID
+                    rating
+                  }
+                }
+              }
+            }
           }
-        }
-      `,
-      variables: {
-        input: {
-          userID: this.user?.id,
-          adID: this.ad?.id,
-          rating,
+        `,
+        variables: {
+          input: {
+            id: this.currentUserRating?.id,
+            userID: this.user.id,
+            adID: this.ad.id,
+            rating,
+          },
         },
-      },
-    });
+      }) as Promise<{ data: { updateRating: Rating | null } }>;
+
+      from(request).subscribe({
+        next: ({ data: { updateRating } }) => {
+          this.ad.ratings = updateRating?.ad?.ratings;
+          this.loading = false;
+          this.notificationsService.success('Rating submitted successfully!');
+        },
+        error: () => {
+          this.notificationsService.error('Something went wrong! Please try again later.');
+        },
+      });
+    } else {
+      const request = API.graphql({
+        query: gql`
+          mutation CreateRating($input: CreateRatingInput!) {
+            createRating(input: $input) {
+              ad {
+                ratings {
+                  items {
+                    id
+                    userID
+                    rating
+                  }
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          input: {
+            userID: this.user.id,
+            adID: this.ad.id,
+            rating,
+          },
+        },
+      }) as Promise<{ data: { createRating: Rating | null } }>;
+
+      from(request).subscribe({
+        next: ({ data: { createRating } }) => {
+          this.ad.ratings = createRating?.ad?.ratings;
+          this.loading = false;
+          this.notificationsService.success('Rating submitted successfully!');
+        },
+        error: () => {
+          this.notificationsService.error('Something went wrong! Please try again later.');
+        },
+      });
+    }
   }
 }
