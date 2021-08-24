211e70a932f35c7b11ab02280da19ed4b9ef411b
Alexander Stoev <stoev95pd@gmail.com>
2021-08-24 21:32:59 +0300

    Favorite component done

--- src/app/modules/admin/components/favourites/favourites.component.ts
+++ src/app/modules/admin/components/favourites/favourites.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Ad, User } from 'src/API';
+import { AdsService } from 'src/app/modules/core/services/ads.service';
+import { FavoriteService } from 'src/app/modules/core/services/favorite.service';
+import { AuthFacade } from 'src/app/store/facades/auth.facade';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.component.html',
-  styleUrls: ['./favourites.component.sass']
+  styleUrls: ['./favourites.component.sass'],
 })
-export class FavouritesComponent implements OnInit {
+export class FavouritesComponent implements OnInit, OnDestroy {
+  private readonly subscriptions: Subscription[] = [];
+  public ads$: Observable<Ad[]> | undefined;
+  public host: string | undefined;
+  public user: User | undefined;
 
-  constructor() { }
+  constructor(
+    private readonly authFacade: AuthFacade,
+    private readonly elemRef: ElementRef,
+    private readonly favoriteService: FavoriteService
+  ) {}
 
   ngOnInit(): void {
+    this.host = this.elemRef.nativeElement.tagName.toLowerCase();
+
+    this.subscriptions.push(
+      this.authFacade.user$.subscribe((user) => {
+        if (user) {
+          this.user = user;
+          this.ads$ = this.favoriteService.loadFavorites(user.id);
+        }
+      })
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
 }
