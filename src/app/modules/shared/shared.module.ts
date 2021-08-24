cc1c39f716b686d929fbd8702d3058ab572d18b3
Alexander Stoev <stoev95pd@gmail.com>
2021-08-24 21:32:01 +0300

    Confirm dialog created

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatRippleModule } from '@angular/material/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { IvyCarouselModule } from 'angular-responsive-carousel';
 // Components
 import { AdCardBigComponent } from './components/ad-card-big/ad-card-big.component';
 import { AdCardSmallComponent } from './components/ad-card-small/ad-card-small.component';
@@ -12,10 +8,15 @@ import { LoadingComponent } from './components/loading/loading.component';
 import { NoResultsComponent } from './components/no-results/no-results.component';
 import { RatingComponent } from './components/rating/rating.component';
 import { SocialFabIconComponent } from './components/social-fab-icon/social-fab-icon.component';
+import { AddToFavorite } from './components/add-to-favorite/add-to-favorite.component';
 // Pipes
 import { SortRegionsPipe } from '../main/pipes/sort-regions.pipe';
 import { CurrencyPipe } from './pipes/currency.pipe';
 // Modules
+import { MatRippleModule } from '@angular/material/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -24,7 +25,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { RouterModule } from '@angular/router';
-import { AddToFavorite } from './components/add-to-favorite/add-to-favorite.component';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 const COMPONENTS = [
   AdCardBigComponent,
@@ -53,11 +55,12 @@ const MODULES = [
   MatOptionModule,
   MatSelectModule,
   RouterModule,
+  MatDialogModule,
 ];
 
 @NgModule({
   imports: [...MODULES],
-  declarations: [...COMPONENTS],
+  declarations: [...COMPONENTS, ConfirmDialogComponent],
   exports: [...COMPONENTS, ...MODULES],
 })
 export class SharedModule {}
