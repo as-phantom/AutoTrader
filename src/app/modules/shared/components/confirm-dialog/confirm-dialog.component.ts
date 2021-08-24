8a83209a4c455ce26bbc6974ba86e8fb136c1f53
Alexander Stoev <stoev95pd@gmail.com>
2021-08-24 21:28:25 +0300

    Confirm dialog created

---
+++ src/app/modules/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.sass'],
+})
+export class ConfirmDialogComponent implements OnInit {
+  @Output() public readonly confirm: EventEmitter<void> = new EventEmitter();
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { message: string },
+    private readonly matDialogRef: MatDialogRef<ConfirmDialogComponent>
+  ) {}
+
+  ngOnInit(): void {}
+
+  public onCancel(): void {
+    this.matDialogRef.close();
+  }
+
+  public onConfirm(): void {
+    this.matDialogRef.close();
+    this.confirm.emit();
+  }
+}
